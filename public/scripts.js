07e90a61aa3f1c27695855a16fc08303487bde52
HugoTeixeira <hugoteixeira0710@gmail.com>
2021-12-07 12:36:55 -0100

    alterAula 7/12

--- public/scripts.js
+++ public/scripts.js
@@ -22,10 +22,67 @@ function getTipo(){
     .then(data => {
         for(let i=0; i<data.length; i++){
             const op = 
-            `<option>${data[i].designacao}</option>`
+            `<option value = "${data[i].idtipos}">${data[i].designacao}</option>`
             tipos.innerHTML += op
 
         }
     })
     .catch()
+}
+
+function mostrarAlunos(){
+    let nome=document.getElementById('nome_aluno').value
+    if(nome==''){
+        alert('O campo não pode estar vazio')
+    }
+
+    let morada=document.getElementById('morada_rua').value
+    if(morada==''){
+        alert('O campo não pode estar vazio')
+    }
+
+    let morada_num=document.getElementById('morada_num').value
+    if(morada_num==''){
+        alert('O campo não pode estar vazio')
+    }
+
+    let dnasc=document.getElementById('dnasc').value
+    if(dnasc==''){
+        alert('O campo não pode estar vazio')
+    } 
+
+    let telem=document.getElementById('telem').value
+    if(telem==''){
+        alert('O campo não pode estar vazio')
+    }else{
+        let i
+        for(let i=0; i<telem.length;i++){
+            if(telem.charAt(i).isNaN){
+                let c = telem.charAt(i)
+                console.log(c)
+                if(isNaN(c)){
+                    console.log('número inválido')
+                    break
+                }
+            }
+
+        }
+        console.log(i)
+        if(i==telem.length){
+            const telemInt = parseInt(telem)
+            console.log(telemInt)
+        }
+    }
+
+    let email=document.getElementById('email').value
+    if(email==''){
+        alert('O campo não pode estar vazio')
+    }
+
+    let tipo=document.getElementById('tipo').value
+    if(tipo==''){
+        alert('O campo não pode estar vazio')
+    }
+    
+
 }
